81f81975c6edd40e4492d5758d7af3f727be4973
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2017-10-24 22:31:01 +0530

    Added template files

--- DBS_FLASK_FILES/DBS/app.py
+++ DBS_FLASK_FILES/DBS/app.py
@@ -65,32 +65,56 @@ def registrationForm():
 
 @app.route("/electronics/mobile")
 def electronics_mobile():
-	return render_template('electronics_mobile.html')
+	return render_template('mobile.html')
 
 @app.route("/electronics/mobile_accessories")
 def electronics_mobile_accessories():
-	return render_template('electronics_mobile_accessories.html')
+	return render_template('mobile_accessories.html')
 
 @app.route("/electronics/laptops")
 def electronics_laptops():
-	return render_template('electronics_laptops.html')
+	return render_template('laptops.html')
 
 @app.route("/electronics/televisions")
 def electronics_televisions():
-	return render_template('electronics_televisions.html')
+	return render_template('televisions.html')
 
-@app.route("/clothingPage")
-def clothingPage():
-	return render_template('clothingPage_men.html')
+@app.route("/clothingPage/men")
+def men():
+	return render_template('men.html')
 
-@app.route("/home_furniturePage")
-def home_furniturePage():
-	return render_template('home_furniturePage.html')
+@app.route("/clothingPage/women")
+def women():
+	return render_template('women.html')
 
-@app.route("/books_morePage")
-def books_morePage():
-	return render_template('books_morePage.html')
+@app.route("/clothingPage/baby&kids")
+def baby_kids():
+	return render_template('baby_kids.html')
 
+@app.route("/home_furniture/kitchen&dining")
+def kitchen_dining():
+	return render_template('kitchen_dining.html')
+
+@app.route("/home_furniture/furniture")
+def furniture():
+	return render_template('furniture.html')
+
+@app.route("/home_furniture/home_decor")
+def home_decor():
+	return render_template('home_decor.html')
+
+@app.route("/books_morePage/books")
+def books():
+	return render_template('books.html')
+
+@app.route("/books_morePage/stationary")
+def stationary():
+	return render_template('stationary.html')
+
+@app.route("/books_morePage/gaming&accessories")
+
+def gaming_accessories():
+	return render_template('gaming_accessories.html')
 
 
 
